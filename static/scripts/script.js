6f18f7b39ddc694f9e8896e949865b7d65376987
Paul Haddad <paulh16@gmail.com>
2017-11-27 09:31:15 -0500

    Get post details toggle working

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,9 @@
+const togglePostDetails = (evt) => {
+  const target = evt.target;
+  const postDetails = target.parentElement.querySelector('.post-details');
+  postDetails.classList.toggle('hidden');
+};
+
 const incrementLikeCount = (evt) => {
   evt.preventDefault();
   const target = evt.target;
@@ -54,17 +60,19 @@ const fetchPosts = () => {
       const postItem = document.createElement('li');
       postItem.setAttribute('data-post-id', id);
 
-      postItem.innerHTML = `${title} by ${author}
-      ${body}
-      <span>${likes}</span> Likes
+      postItem.innerHTML = `<span class='post-header'>${title} by ${author}</span>
+      <span class='post-details hidden'>${body}
+      <span class='like'>${likes}</span> Likes
       <button name='like'>Like this post!</button>
-      <button name='delete'>Delete</button>`;
+      <button name='delete'>Delete</button></span>`;
 
       postsList.appendChild(postItem);
 
       const currentPost = document.querySelector(`li[data-post-id='${id}']`);
+      const postHeader = currentPost.querySelector('.post-header');
       const likeButton = currentPost.querySelector("button[name='like']");
       const deleteButton = currentPost.querySelector("button[name='delete']");
+      postHeader.addEventListener('click', togglePostDetails, false);
       likeButton.addEventListener('click', incrementLikeCount, false);
       deleteButton.addEventListener('click', deletePost, false);
     })
